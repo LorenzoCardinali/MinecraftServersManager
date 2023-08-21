21e9a9dce6baa634c2c21e034483ee07286d226b
Lorenzo Cardinali <lorenzo.cardinali.3@gmail.com>
2023-08-21 21:40:10 +0200

    first commit

---
+++ Start.sh
@@ -0,0 +1,97 @@
+#!/bin/bash
+  ###### ######## ####### #######  ###  #######
+###           ##       ##      ## ###         
+###      #######  ######  ###  ## ###  #######
+###      ###  ##  ##  ##  ###  ## ###  ##     
+ ######  ###  ##  ##   ## ######  ###  ##       
+
+# arguments
+SERVER=$1
+JSON_FILE=$2
+
+###########
+# Imports #
+###########
+
+# status file import
+STATUS_FILE=$(jq -r ".Servers.$SERVER.status_file" "$JSON_FILE")
+
+# statuses import
+declare -A STATUSES=(
+	["on"]="$(jq -r ".Statuses.on" "$JSON_FILE")"
+	["run"]="$(jq -r ".Statuses.run" "$JSON_FILE")"
+	["res"]="$(jq -r ".Statuses.res" "$JSON_FILE")"
+	["off"]="$(jq -r ".Statuses.off" "$JSON_FILE")"
+)
+
+# log file import
+LOG_FILE=$(jq -r ".Servers.$SERVER.logs_file" "$JSON_FILE")
+
+# ram import
+MIN_RAM=$(jq -r ".Servers.${SERVER}.min_ram" "$JSON_FILE")
+MAX_RAM=$(jq -r ".Servers.${SERVER}.max_ram" "$JSON_FILE")
+
+# parameters import
+PARAMETER_ID=$(jq -r ".Servers.${SERVER}.parameter_id" "$JSON_FILE")
+PARAMETERS=$(jq -r ".Parameters[${PARAMETER_ID}]" "$JSON_FILE")
+
+# Jar import 
+JAR_FILE=$(jq -r ".Servers.${SERVER}.jar_file" "$JSON_FILE")
+if [ "$JAR_FILE" == null ]
+then
+	JAR_FILE=""
+else
+	JAR_FILE="-jar ${JAR_FILE}"
+fi
+
+#############
+# Functions #
+#############
+
+# logging handling
+function to_log() {
+	echo "[$(date)] : ${1}" >> "$LOG_FILE"
+}
+
+# change the server status
+function change_status() {
+	echo "$1" > "$STATUS_FILE"
+}
+
+# move to server directory if present
+cd "$SERVER" || exit 1
+
+while true
+do
+	case $(cat "$STATUS_FILE")
+	in
+		"${STATUSES[on]}")
+			to_log "Server started."
+			change_status "${STATUSES[run]}"
+			java17 -Xmx${MAX_RAM} -Xms${MIN_RAM} ${PARAMETERS} $JAR_FILE -nogui
+		;;
+
+		"${STATUSES[run]}")
+			to_log "Server closed or crashed, restarting it..."
+			change_status "${STATUSES[on]}"
+		;;
+
+		"${STATUSES[res]}")
+			to_log "Server restarted."
+			change_status "${STATUSES[on]}"
+		;;
+
+		"${STATUSES[off]}")
+			to_log "Server stopped."
+			rm "$STATUS_FILE"
+			exit 0
+		;;
+
+		*)
+			to_log "ERROR Start script."
+			to_log "Status file -> $(cat "$STATUS_FILE")"
+			rm "$STATUS_FILE"
+			exit 1
+		;;
+	esac
+done
---
+++ server.sh
@@ -0,0 +1,208 @@
+#!/bin/bash
+  ###### ######## ####### #######  ###  #######
+###           ##       ##      ## ###
+###      #######  ######  ###  ## ###  #######
+###      ###  ##  ##  ##  ###  ## ###  ##
+ ######  ###  ##  ##   ## ######  ###  ##
+
+# move to script directory
+cd "$(dirname "${BASH_SOURCE[0]}")" || exit 1
+
+# arguments
+SERVER=${1}
+COMMAND=${2}
+ARG=${3}
+
+# Json and script
+START_SCRIPT="Start.sh"
+JSON_FILE="Config.json"
+
+# error handling
+function error() {
+	echo "ERROR: $1"
+	exit 1
+}	
+
+# file presents check
+function is_present() {
+	test -f "$1"
+}
+
+# help
+function help() {
+	echo "ERROR: $1"
+	echo "Script usage: ${0} [Server name] [Command]"
+	echo "Commands list:"
+	echo -e "start \011\011- start server"
+	echo -e "stop \011\011- stop server"
+	echo -e "restart \011- restart server"
+	echo -e "status \011\011- display the server status"
+	echo -e "console \011- open the server console"
+	echo -e "broad [string] \011- broadcast a string"
+	echo -e "cmd [string] \011- execute a command"
+	exit
+}
+
+######################
+# Imports and checks #
+######################
+
+# start script check
+if ! is_present $START_SCRIPT 
+then 
+	error "Start script not present."
+fi
+
+# Json and Server check
+if is_present $JSON_FILE
+then
+	if [ "$SERVER" == "" ]
+	then
+		help "invalid Server name"
+	elif ! jq -r ".Servers | keys" $JSON_FILE | grep -q "$SERVER"
+	then
+		error "Server not present."
+	fi
+else
+	error "Json file not present."
+fi
+
+# Jar import and check
+JAR_FILE=$(jq -r ".Servers.${SERVER}.jar_file" "$JSON_FILE")
+if [ ! $JAR_FILE == null ]
+then
+ 	if ! is_present "$SERVER/$JAR_FILE"
+	then
+		error "Jar file not present or incorrect."
+	fi
+fi
+
+# log file import and check
+LOG_FILE="$SERVER/$(jq -r ".Servers.$SERVER.logs_file" "$JSON_FILE")"
+if ! is_present "$LOG_FILE"
+then
+	echo "Log file not present, making a new one..."
+	touch "$LOG_FILE"
+fi
+
+# status file import
+STATUS_FILE="$SERVER/$(jq -r ".Servers.$SERVER.status_file" "$JSON_FILE")"
+
+# statuses import
+declare -A STATUSES=(
+	["on"]="$(jq -r ".Statuses.on" "$JSON_FILE")"
+	["run"]="$(jq -r ".Statuses.run" "$JSON_FILE")"
+	["res"]="$(jq -r ".Statuses.res" "$JSON_FILE")"
+	["off"]="$(jq -r ".Statuses.off" "$JSON_FILE")"
+)
+
+# screen parameters
+SCREEN_NAME="MC_${SERVER}"
+
+#############
+# Functions #
+#############
+
+# server and screen start
+function server_start() {
+	screen -Sdm $SCREEN_NAME ./$START_SCRIPT "$SERVER" "$JSON_FILE"
+}
+
+# check if screen exist
+function screen_check() {
+	screen -list | grep -q "${SCREEN_NAME}"
+}
+
+# execute a command in the server console
+function to_console() {
+	screen -S ${SCREEN_NAME} -X stuff "${1}\015"
+}
+
+# change the server status
+function change_status() {
+	echo "$1" > "$STATUS_FILE"
+}
+
+##################
+# Commands cases #
+##################
+
+case $COMMAND
+in
+	start)
+		if screen_check
+		then
+			error "Server is already running..."
+		else
+			echo "Server starting..."
+			change_status "${STATUSES[on]}"
+			server_start
+ 		fi
+	;;
+
+  	stop)
+		if screen_check
+		then
+			echo "Server stopping..."
+			change_status "${STATUSES[off]}"
+			to_console "say Stopping the server in 5 seconds."
+        	sleep 5
+			to_console "stop"
+		else
+			error "Server is already stopped..."
+		fi
+   	;;
+
+	restart)
+		if screen_check
+		then
+			echo "Server restarting..."
+			change_status "${STATUSES[res]}"
+			to_console "say Restarting the server in 5 seconds."
+        	sleep 5
+			to_console "stop"
+		else
+			error "Server is not active..."
+		fi
+   	;;
+
+	console)
+		if screen_check
+		then
+			screen -r "$SCREEN_NAME"
+		else
+			error "Server is not active..."
+		fi
+	;;
+
+	broad)
+		if screen_check
+		then
+			to_console "say $ARG"
+		else
+			error "Server is not active..."
+		fi
+	;;
+
+	cmd)
+		if screen_check
+		then
+			to_console "$ARG"
+		else
+			error "Server is not active..."
+		fi
+	;;
+
+	status)
+		if screen_check
+		then
+			cat "$STATUS_FILE"
+		else
+			echo "${STATUSES[off]}"
+		fi
+	;;
+
+  	*)
+		help "invalid command"
+    ;;
+esac
