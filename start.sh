68c477deb2e40cb8fe5305460d9e8f54bda4b7d8
Lorenzo Cardinali <lorenzo.cardinali.3@gmail.com>
2023-10-25 10:14:36 +0200

    fix: can not start server with -nogui argument

--- start.sh
+++ start.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-  ###### ######## ####### #######  ###  #######
-###           ##       ##      ## ###         
+###### ######## ####### #######  ###  #######
+###           ##       ##      ## ###
 ###      #######  ######  ###  ## ###  #######
-###      ###  ##  ##  ##  ###  ## ###  ##     
- ######  ###  ##  ##   ## ######  ###  ##       
+###      ###  ##  ##  ##  ###  ## ###  ##
+######  ###  ##  ##   ## ######  ###  ##
 
 # arguments
 SERVER=$1
@@ -18,11 +18,11 @@ STATUS_FILE=$(jq -r ".files.status_file" "$JSON_FILE")
 
 # statuses import
 declare -A STATUS=(
-	["on"]="$(jq -r ".statuses.on" "$JSON_FILE")"
-	["run"]="$(jq -r ".statuses.run" "$JSON_FILE")"
-	["res"]="$(jq -r ".statuses.res" "$JSON_FILE")"
-	["off"]="$(jq -r ".statuses.off" "$JSON_FILE")"
-	["err"]="$(jq -r ".statuses.err" "$JSON_FILE")"
+    ["on"]="$(jq -r ".statuses.on" "$JSON_FILE")"
+    ["run"]="$(jq -r ".statuses.run" "$JSON_FILE")"
+    ["res"]="$(jq -r ".statuses.res" "$JSON_FILE")"
+    ["off"]="$(jq -r ".statuses.off" "$JSON_FILE")"
+    ["err"]="$(jq -r ".statuses.err" "$JSON_FILE")"
 )
 
 # log file import
@@ -36,13 +36,13 @@ MAX_RAM=$(jq -r ".servers.${SERVER}.max_ram" "$JSON_FILE")
 PARAMETER_ID=$(jq -r ".servers.${SERVER}.parameter_id" "$JSON_FILE")
 PARAMETERS=$(jq -r ".parameters[${PARAMETER_ID}]" "$JSON_FILE")
 
-# Jar import 
+# Jar import
 JAR_FILE=$(jq -r ".servers.${SERVER}.jar_file" "$JSON_FILE")
 if [ "$JAR_FILE" == null ]
 then
-	JAR_FILE=""
+    JAR_FILE=""
 else
-	JAR_FILE="-jar ${JAR_FILE}"
+    JAR_FILE="-jar ${JAR_FILE}"
 fi
 
 #############
@@ -51,12 +51,12 @@ fi
 
 # logging handling
 function fn_to_log() {
-	echo "[$(date)] : ${1}" >> "$LOG_FILE"
+    echo "[$(date)] : ${1}" >> "$LOG_FILE"
 }
 
 # change the server status
 function fn_change_status() {
-	echo "$1" > "$STATUS_FILE"
+    echo "$1" > "$STATUS_FILE"
 }
 
 # move to server directory if present
@@ -73,18 +73,18 @@ TEST_TIME=500
 TRIES=$MAX_TRIES
 
 function fn_timer_update() {
-	if [ $(($(date +%s) - "$TIME_STAMP")) -ge $TEST_TIME ]
-	then
-		TIME_STAMP=$(date +%s)
-		TRIES=$MAX_TRIES
-	else
-		((TRIES -= 1))
-	fi
-
-	if [ $TRIES -le 0 ]
-	then
-		fn_change_status "${STATUS[err]}"
-	fi
+    if [ $(($(date +%s) - "$TIME_STAMP")) -ge $TEST_TIME ]
+    then
+        TIME_STAMP=$(date +%s)
+        TRIES=$MAX_TRIES
+    else
+        ((TRIES -= 1))
+    fi
+    
+    if [ $TRIES -le 0 ]
+    then
+        fn_change_status "${STATUS[err]}"
+    fi
 }
 
 #######################
@@ -93,40 +93,40 @@ function fn_timer_update() {
 
 while true
 do
-	case $(cat "$STATUS_FILE")
-	in
-		"${STATUS[on]}")
-			fn_to_log "Server started."
-			fn_change_status "${STATUS[run]}"
-			fn_timer_update
-			java -Xmx${MAX_RAM} -Xms${MIN_RAM} ${PARAMETERS} ${JAR_FILE} -nogui
-		;;
-
-		"${STATUS[run]}")
-			fn_to_log "Server closed or crashed, restarting it..."
-			fn_change_status "${STATUS[on]}"
-		;;
-
-		"${STATUS[res]}")
-			fn_to_log "Server restarted."
-			fn_change_status "${STATUS[on]}"
-		;;
-
-		"${STATUS[off]}")
-			fn_to_log "Server stopped."
-			exit 0
-		;;
-
-		"${STATUS[err]}")
-			fn_to_log "Server crashed multiple times, shutting it down..."
-			fn_change_status "${STATUS[off]}"
-		;;
-
-		*)
-			fn_to_log "ERROR Start script."
-			fn_to_log "Status file -> $(cat "$STATUS_FILE")"
-			rm "$STATUS_FILE"
-			exit 1
-		;;
-	esac
+    case $(cat "$STATUS_FILE")
+        in
+        "${STATUS[on]}")
+            fn_to_log "Server started."
+            fn_change_status "${STATUS[run]}"
+            fn_timer_update
+            java -Xmx${MAX_RAM} -Xms${MIN_RAM} ${PARAMETERS} ${JAR_FILE}
+        ;;
+        
+        "${STATUS[run]}")
+            fn_to_log "Server closed or crashed, restarting it..."
+            fn_change_status "${STATUS[on]}"
+        ;;
+        
+        "${STATUS[res]}")
+            fn_to_log "Server restarted."
+            fn_change_status "${STATUS[on]}"
+        ;;
+        
+        "${STATUS[off]}")
+            fn_to_log "Server stopped."
+            exit 0
+        ;;
+        
+        "${STATUS[err]}")
+            fn_to_log "Server crashed multiple times, shutting it down..."
+            fn_change_status "${STATUS[off]}"
+        ;;
+        
+        *)
+            fn_to_log "ERROR Start script."
+            fn_to_log "Status file -> $(cat "$STATUS_FILE")"
+            rm "$STATUS_FILE"
+            exit 1
+        ;;
+    esac
 done
